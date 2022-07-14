a55bf53d99ad5f1eac2aad2939b572680e2b6ff0
Amal221200 <92168224+Amal221200@users.noreply.github.com>
2022-07-14 13:06:09 +0530

    Initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,49 @@
+import { useSelector, useDispatch } from "react-redux/es/exports";
+import { increment, decrement, incrementByAmount } from "./redux/counter";
+
+const Counter = () => {
+  // const [count, setCount] = useState(0);
+  // const count = useSelector((state) => state.counter.count)
+  // OR
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div
+      style={{
+        backgroundColor: "grey",
+        height: "125px",
+        padding: "10px",
+        minWidth: "250px"
+      }}
+    >
+      <h3>Count : {count}</h3>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => {
+            dispatch(incrementByAmount(30));
+          }}
+        >
+          Increment by 30
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
